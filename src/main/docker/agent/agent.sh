04fbd5b464a32665e87bb5506b03bb4a3ffb256d
henning.gross <igaffai@gmail.com>
2014-11-17 20:16:16 +0100

    host ip

--- src/main/docker/agent/agent.sh
+++ src/main/docker/agent/agent.sh
@@ -23,7 +23,7 @@ if [ ! -d "$AGENT_DIR" ]; then
     echo "tempDir=../temp" >> $AGENT_DIR/conf/buildAgent.properties
     echo "systemDir=../system" >> $AGENT_DIR/conf/buildAgent.properties
     # netstat -nr | grep '^0\.0\.0\.0' | awk '{print $2}'`
-    echo "system.HOST_IP=`/sbin/ip route|awk '/default/ { print $3 }'`" >> $AGENT_DIR/conf/buildAgent.properties
+    echo "env.HOST_IP=`/sbin/ip route|awk '/default/ { print $3 }'`" >> $AGENT_DIR/conf/buildAgent.properties
 else
     echo "Using agent at ${AGENT_DIR}."
 fi
